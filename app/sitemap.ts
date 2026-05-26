90e18700258ba5ebbe2b18fd6978ddff3b1d9954
Manytuch Mangar <manytuch123@gmail.com>
2026-05-26 16:01:30 +0530

    sitemap & robots

---
+++ app/sitemap.ts
@@ -0,0 +1,56 @@
+export default function sitemap() {
+  return [
+    // HOME
+    {
+      url: "https://lifecyclegeneral.com",
+      lastModified: new Date(),
+    },
+
+    // MAIN PAGES
+    {
+      url: "https://lifecyclegeneral.com/about",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://lifecyclegeneral.com/operations",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://lifecyclegeneral.com/why-choose-us",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://lifecyclegeneral.com/certificates",
+      lastModified: new Date(),
+    },
+
+    // SERVICE PAGES
+    {
+      url: "https://lifecyclegeneral.com/services/customs-clearance",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://lifecyclegeneral.com/services/shipping-transportation",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://lifecyclegeneral.com/services/warehousing",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://lifecyclegeneral.com/services/brokerage-services",
+      lastModified: new Date(),
+    },
+
+    {
+      url: "https://lifecyclegeneral.com/services/general-supply",
+      lastModified: new Date(),
+    },
+  ];
+}
